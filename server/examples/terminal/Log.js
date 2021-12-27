333d84ceb5baa0e45c00662fce18f941428b7b0c
XFaon <xfaonae@protonmail.com>
2021-12-27 00:06:48 -0500

    Fix: TypeScript compiler not compiling anything Chore: Bumped pre release to 2

---
+++ server/examples/terminal/Log.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const main_1 = require("../../src/main");
+main_1.terminal.info("This is an info message");
+main_1.terminal.error("This is an error message");
+main_1.terminal.warning("This is a warning message");
+main_1.terminal.success("This is a success message");
+main_1.terminal.setBulletMode(false);
+main_1.terminal.setTimeStampMode(true);
+main_1.terminal.info("This is an info message");
+main_1.terminal.error("This is an error message");
+main_1.terminal.warning("This is a warning message");
+main_1.terminal.success("This is a success message");
+//# sourceMappingURL=Log.js.map
