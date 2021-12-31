68c814b5ba468395b3fc4ad5718af9061ed985da
XFaon <xfaonae@protonmail.com>
2021-12-30 19:58:06 -0500

    Client/Engines/Theming Chore: Added theming engine's logic

--- client/src/engines/theming.ts
+++ client/src/engines/theming.ts
@@ -1,3 +1,5 @@
+import { utils } from "../main";
+
 export interface Theme {
     /**
      * The type of theme
@@ -7,7 +9,12 @@ export interface Theme {
     /**
      * The theme's author
      */
-    author?: "string";
+    author: string;
+
+    /**
+     * The name of the theme
+     */
+    name: string;
 
     /**
      * The theme's description
@@ -40,11 +47,146 @@ export interface Theme {
     license?: string;
 }
 
+export interface ThemeCSSPrettySettings {
+    /**
+     * The indentation character
+     */
+    indentChar?: string;
+
+    /**
+     * The new line character
+     */
+    newLineChar?: string;
+}
+
+let themeStore = {} as {
+    [ index: string ]: {
+        [ index: string ]: Theme;
+    };
+};
+
+/**
+ * Generate a theme using CSS variables from a theme palette
+ * @param theme The theme palette
+ * @param pretty Make the output pretty
+ */
+export function buildCSSFromPalette(theme: { [ index: string ]: string }, pretty: boolean | ThemeCSSPrettySettings = false): string {
+    let result = ":root {";
+    let prettySettings = null as ThemeCSSPrettySettings | null;
+
+    if (pretty == true) {
+        pretty = {};
+    }
+
+    if (pretty) {
+        prettySettings = utils.mergeObject<ThemeCSSPrettySettings>({
+            indentChar: "    ",
+            newLineChar: "\n"
+        }, pretty);
+    }
+
+    if (prettySettings) {
+        result += prettySettings.newLineChar;
+    }
+
+    for (const themeVar in theme) {
+        if (prettySettings) {
+            result += `${prettySettings.indentChar}--${themeVar}: ${theme[themeVar]};${prettySettings.newLineChar}`;
+        } else {
+            result += `--${themeVar}: ${theme[themeVar]};`;
+        }
+    }
+
+    return result + "}";
+}
+
+export enum ThemeInstallErrors {
+    /**
+     * A theme under that author already exists
+     */
+    themeExists,
+
+    /**
+     * The name of the theme contains spaces
+     */
+    nameContainsSpaces,
+
+    /**
+     * The name of the author contains spaces
+     */
+    authorContainsSpaces
+}
+
 /**
  * Install a theme to local storage
  * @param theme The theme
- * @returns The theme's ID for when uninstalling it
+ * @returns A promise for when the theme is installed
  */
-export function installTheme(theme: Theme): string {
-    return "un imp"
+export function installTheme(theme: Theme): Promise<void> {
+    return new Promise((resolve, reject) => {
+        if (themeStore[theme.author] && themeStore[theme.author][theme.name]) {
+            reject(ThemeInstallErrors.themeExists);
+            return;
+        }
+
+        if (theme.name.includes(" ")) {
+            reject(ThemeInstallErrors.nameContainsSpaces);
+            return;
+        }
+
+        if (theme.author.includes(" ")) {
+            reject(ThemeInstallErrors.authorContainsSpaces);
+            return;
+        }
+
+        if (themeStore[theme.author] == undefined) {
+            themeStore[theme.author] = {};
+        }
+
+        themeStore[theme.author][theme.name] = theme;
+        resolve();
+    });
+}
+
+export enum ThemeLoadErrors {
+    /**
+     * A theme on that author was not installed
+     */
+    nameNotFound,
+
+    /**
+     * There are no themes using that author currently installed
+     */
+    authorNotFound
+}
+
+/**
+ * Load a theme for rendering
+ * @param author Theme author
+ * @param name Theme name
+ * @returns Promise for when the theme is loaded
+ */
+export function loadTheme(author: string, name: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        if (themeStore[author] == undefined) {
+            reject(ThemeLoadErrors.authorNotFound);
+            return;
+        }
+
+        if (themeStore[author][name] == undefined) {
+            reject(ThemeLoadErrors.nameNotFound);
+            return;
+        }
+
+        const themeRenderingArea = document.head;
+        let themeStorageElement = document.createElement("style");
+        themeStorageElement.setAttribute("type", "text/css");
+
+        if (themeRenderingArea.querySelector("__luxjs__theme__css")) {
+            themeRenderingArea.querySelector("__luxjs__theme__css")?.remove();
+        }
+
+        themeStorageElement.innerHTML = buildCSSFromPalette(themeStore[author][name].palette);
+        themeRenderingArea.appendChild(themeStorageElement);
+    });
 }
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,5 @@
 import Button from "./controls/button/Button";
 import * as utils from "./utils/utils";
+import * as theming from "./engines/theming";
 
-export { utils, Button };
+export { utils, Button, theming };
