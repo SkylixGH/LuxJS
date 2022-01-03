7d84476dee178bc350a6839632d11cbf3b2c9095
XFaon <xfaonae@protonmail.com>
2022-01-02 22:11:09 -0500

    Chore: Updated example

--- server/examples/terminal/Log.ts
+++ server/examples/terminal/Log.ts
@@ -1,5 +1,7 @@
 import { terminal } from "../../src/main";
 
+terminal.setBulletMode(true);
+
 terminal.info("This is an info message");
 terminal.error("This is an error message");
 terminal.warning("This is a warning message");
@@ -7,6 +9,13 @@ terminal.success("This is a success message");
 
 terminal.setBulletMode(false);
 terminal.setTimeStampMode(true);
+
+terminal.info("This is an info message");
+terminal.error("This is an error message");
+terminal.warning("This is a warning message");
+terminal.success("This is a success message");
+
+terminal.setTimeStampMode(false);
 
 terminal.info("This is an info message");
 terminal.error("This is an error message");
