a7c88b7b98d85b61fa0dbfde5e029904204a455f
XFaonAE <xfaonae@protonmail.com>
2022-01-04 09:13:50 -0500

    Props: Added more props to `ScrollPane`

--- client/src/controls/scrollPane/ScrollPane.tsx
+++ client/src/controls/scrollPane/ScrollPane.tsx
@@ -6,16 +6,63 @@ interface Props {
     /**
      * Scroll pane contents
      */
-    children?: JSX.Element | JSX.Element[];
+    children: JSX.Element | JSX.Element[];
+
+    /**
+     * CSS width
+     */
+    width?: string;
+
+    /**
+     * Max CSS width
+     */
+    maxWidth?: string;
+
+    /**
+     * Min CSS width
+     */
+    minWidth?: string;
+
+    /**
+     * CSS height
+     */
+    height?: string;
+
+    /**
+     * Max CSS height
+     */
+    maxHeight?: string;
+
+    /**
+     * Min CSS height
+     */
+    minHeight?: string;
 }
 
 export interface RefInstance {}
 
 const ScrollPane = React.forwardRef<RefInstance, Props>((props, ref) => {
-    props = utils.mergeObject<Props>({}, props);
+    props = utils.mergeObject<Props>({
+        children: <></>,
+        width: "100%",
+        height: "100px",
+        maxWidth: "none",
+        minWidth: "none",
+        maxHeight: "100px",
+        minHeight: "none"
+    }, props);
 
     return (
-        <div className={styles._}>{props.children}</div>
+        <div className={styles._}>
+            <div style={{
+                width: props.width,
+                height: props.height,
+                maxHeight: props.maxHeight,
+                maxWidth: props.maxWidth,
+                minWidth: props.minWidth,
+                minHeight: props.minHeight
+            }}>{props.children}</div>
+        </div>
     );
 });
 
