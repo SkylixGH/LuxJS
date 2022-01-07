9db46af2ce8f284381a3cc615ee47a24cb1ce6ea
XFaonAE <xfaonae@protonmail.com>
2022-01-07 13:32:07 -0500

    Utils space

--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -35,7 +35,7 @@ export function jsonParse<ObjectType>(jsonString: string): Promise<ObjectType> {
             resolve(jsonObject);
         } catch (error) {
             reject(error);
-        }
+        } 
     });
 }
 
